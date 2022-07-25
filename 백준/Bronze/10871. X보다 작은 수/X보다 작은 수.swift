0bf28ec3b3bcaaa0c1c45cdf5caf571e2b5555c0
choijaegwon <68246962+choijaegwon@users.noreply.github.com>
2022-07-26 02:43:25 +0900

    [Bronze V] Title: X보다 작은 수, Time: 36 ms, Memory: 79872 KB -BaekjoonHub

---
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+let num1 = readLine()!.components(separatedBy: " ")
+let num2 = readLine()!.components(separatedBy: " ")
+
+for i in num2 {
+    if Int(i)! < Int(num1[1])! {
+        print(i, terminator: " ")
+    }
+}
