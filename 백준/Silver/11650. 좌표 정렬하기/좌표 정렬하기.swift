ece42db6b4855c1629e27ecf9a0746adadbbb9d8
choijaegwon <68246962+choijaegwon@users.noreply.github.com>
2022-08-08 01:56:35 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 232 ms, Memory: 72240 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.swift
@@ -0,0 +1,15 @@
+let num = Int(readLine()!)!
+var arr: [(Int, Int)] = []
+
+for _ in 0..<num {
+    let a = readLine()!.split(separator: " ").map{Int($0)!}
+    arr.append((a[0], a[1]))
+}
+
+arr.sort(by: {
+    $0.0 == $1.0 ? $0.1 < $1.1 : $0.0 < $1.0
+} )
+
+for x in 0..<num {
+    print("\(arr[x].0) \(arr[x].1)")
+}
